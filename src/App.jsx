eeb2c5803467ffcba6e9864275803b962d9f0dd8
Carlos Araya Salfate <carlosarayasalfate@gmail.com>
2022-11-19 14:01:01 -0300

    eliminar archivos y dependencias sin usar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-// import Lista from './components/Lista'
-// import Todos from './components/Todos'
+import { Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import Detalle from './views/Detalle'
-//import reactLogo from './assets/react.svg'
-import axios from 'axios'
-
 
 const  App = () => {
-
-  
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/:name" element={<Detalle />}/>
-        
       </Routes>  
     </div>
-  
   )
 }
 
